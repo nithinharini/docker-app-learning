700b07632adfb5f23581717f5798ef61b1300130
nithingudala26 <nithingudala2026@gmail.com>
2026-09-03 15:48:58 -0500

    updated latest

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, { "Content-Type": "text/html"});
-    response.end("<h1>Dicker AWS Learning - version 2 </h1>");
+    response.end("<h1>Dicker AWS Learning - version 3 </h1>");
 });
 
 server.listen(3000, "0.0.0.0", () => {
