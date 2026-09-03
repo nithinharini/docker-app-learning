96a19eb565a796b64450441f0ae0bad4dea2aa29
nithingudala26 <nithingudala2026@gmail.com>
2026-09-03 15:20:40 -0500

    intional commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/html"});
+    response.end("<h1>Dicker AWS Learning - version 2 </h1>");
+});
+
+server.listen(3000, "0.0.0.0", () => {
+    console.log("Application running on port 3000");
+})
